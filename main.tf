d51fc61dc0e5808b9f2ea13dfa199ddc36f3c6ea
sanjaysn <sanju.sn1922@gmail.com>
2026-03-09 20:07:54 +0530

    first

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-02dfbd4ff395f2a1b"   # Amazon Linux 2 AMI (example for ap-south-1)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Jenkins-Terraform-Server"
+  }
+}
